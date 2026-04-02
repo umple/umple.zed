9af79f6a6201ceb1c7dece02a6700a7d2cbfcbda
flyingbutton <thedrafttin@gmail.com>
2026-04-02 12:00:29 -0400

    Sync grammar rev and highlights with umple-lsp 0943758
    Update tree-sitter grammar pin to latest umple-lsp HEAD covering
    trait SM operations, trace extensions, code injection patterns,
    test DSL, namespace/suboption/distributable directives, PHP params,
    varargs, string concat, inline mixset trait/interface, and more.
    Highlights updated with new keywords.

--- languages/umple/highlights.scm
+++ languages/umple/highlights.scm
@@ -87,6 +87,7 @@
   "abstract"
   "static"
   "const"
+  "constant"
   "lazy"
   "settable"
   "internal"
@@ -109,6 +110,9 @@
   "before"
   "after"
   "around"
+  "custom"
+  "generated"
+  "all"
   "emit"
 ] @keyword.directive
 
@@ -119,13 +123,23 @@
   "active"
   "final"
   "trace"
+  "tracer"
   "tracecase"
+  "suboption"
+  "distributable"
+  "test"
+  "generic"
   "activate"
   "deactivate"
 ] @keyword
 
 ; Trace postfix sub-keywords (children of trace_postfix, not trace_statement)
-(trace_postfix ["where" "until" "after" "giving" "record"] @keyword)
+; Trace prefix keywords (children of trace_statement)
+(trace_statement ["set" "get" "in" "out" "entry" "exit" "cardinality" "add" "remove"] @keyword)
+(trace_postfix ["where" "until" "after" "giving" "record" "logLevel" "for"] @keyword)
+(trace_postfix ["trace" "debug" "info" "warn" "error" "fatal" "all" "finest" "fine" "config" "warning" "severe"] @constant)
+; Tracer directive type
+(tracer_directive type: (identifier) @type)
 ; activate/deactivate modifiers (direct children of trace_statement)
 (trace_statement ["onAllObjects" "onThisThreadOnly" "onThisObject"] @keyword)
 
@@ -187,7 +201,7 @@
 
 ; Trait SM binding: highlight only (value is visual approximation, not semantic type)
 (trait_sm_binding
-  param: (identifier) @variable.member
+  param: (qualified_name) @variable.member
   value: (qualified_name) @type)
 
 ; Built-in types
